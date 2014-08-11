97d91b77f90f526ddad8fd28fa26edb26390b078
Christian Monaghan <christian.monaghan@gmail.com>
2014-08-10 19:13:38 -0500

    Adds ability to download multiple songs [Closes #1]

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
 ### Here's how to use this tool:
   1. Run `npm install`
-  2. Copy the url of a song on soundcloud that you would like to download
-  3. From the command line, type `node soundcloudDownloader.js [desired song url]`
+  2. Copy the url of a song on soundcloud that you'd like to download
+  3. From the command line, type `node soundcloudDownloader.js '[desired song url]'`
   4. Profit!
 
-### TODO:
-  - check if file to download already exists so it's not downloaded a second time
-  - add ability to download multiple songs at once
-  - create a way to easily run this tool for all songs you've 'liked' on soundcloud
-  - add testing
+### Notes:
+test run:
+
+    node soundcloudDownloader.js 'https://soundcloud.com/jthunder/we-make-it-better-rolling-in' 'https://soundcloud.com/xaanti/my-heart-club-mix'
--- soundcloudDownloader.js
+++ soundcloudDownloader.js
@@ -1,6 +1,9 @@
 // You can modify the directory to download your songs to here:
 var DOWNLOAD_DIR = './'; // make sure this directory has a trailing slash
 
+// API url
+var sounddrainApiUrl = 'http://www.sounddrain.com/sounddrain_api/';
+
 // Dependencies
 var fs = require('fs');
 var url = require('url');
@@ -9,8 +12,6 @@ var path = require('path');
 var request = require('request');
 var exec = require('exec');
 
-var soundcloudSongUrl = process.argv[2]; // grab the url argument from the command line
-var sounddrainApiUrl = 'http://www.sounddrain.com/sounddrain_api/';
 
 // Create the download directory if it doesn't exist
 var mkdir = 'mkdir -p ' + DOWNLOAD_DIR;
@@ -20,22 +21,27 @@ var child = exec(mkdir, function(err, stdout, stderr) {
   }
 });
 
-// call the download function
-downloadSongFromSoundcloud(soundcloudSongUrl);
+// grab the url arguments from the command line
+var songUrls = Array.prototype.slice.call(process.argv, 2);
 
-function downloadSongFromSoundcloud(soundcloudSongUrl) {
+// call the download function for each url in the songUrls array
+for (var i = 0; i < songUrls.length; i++) {
+  console.log('downloading ' + songUrls[i] + ' ...');
+  downloadSongFromSoundcloud(songUrls[i]);
+};
+
+function downloadSongFromSoundcloud(songUrl) {
   // Query the sounddrain API for a link to download the song
   request.post(
-    sounddrainApiUrl, // url
-    { form: {url: soundcloudSongUrl} }, // form data
-    function(error, response, body) { // callback
-      if (error) {
-        console.log('Error:', error);
+    sounddrainApiUrl, // url we're POSTing to
+    { form: {url: songUrl} }, // form data to POST
+    function(err, response, body) { // callback
+      if (err) {
+        console.error(err);
       }
 
-      console.log('status:', response.statusCode);
       body = JSON.parse(body);
-      console.log('The sounddrain API response is:', body);
+      // console.log('The sounddrain API response is:', body);
 
       downloadSong(body.url, body.title);
     }
@@ -47,7 +53,7 @@ function downloadSongFromSoundcloud(soundcloudSongUrl) {
     var fileExt = path.extname( url.parse(songUrl).pathname.split('/').pop() );
     songTitle += fileExt; // adds the file extension to the song title
 
-    // @TODO: check if this file already exists, so we don't download something we already have again
+    // @TODO: check if this file already exists, so we don't re-download something we already have
     var file = fs.createWriteStream(DOWNLOAD_DIR + songTitle);
 
     https.get(songUrl, function(res) {
@@ -55,8 +61,10 @@ function downloadSongFromSoundcloud(soundcloudSongUrl) {
         file.write(data);
       }).on('end', function() {
         file.end();
-        console.log(songTitle + ' downloaded to ' + DOWNLOAD_DIR);
+        console.log('Success! Downloaded ' + songTitle + ' to ' + DOWNLOAD_DIR);
       });
+    }).on('error', function(err) {
+      console.error(err);
     });
   };
 };
