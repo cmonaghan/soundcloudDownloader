4f4976a0677ace93a1ee5a7d40c1329fe4dc6060
Christian Monaghan <christian.monaghan@gmail.com>
2014-10-10 23:32:19 -0700

    Refactors getUserId helper

--- findUserFavorites.js
+++ findUserFavorites.js
@@ -4,10 +4,8 @@ var request = Promise.promisifyAll(require('request')); // converts request to u
 var _ = require('underscore');
 
 function findUserFavorites(username, scClientId) {
-  // finds the integer user id associated with the username
-  var resolveUrl = buildResolveUrl(username, scClientId);
 
-  return getUserId(resolveUrl).then(function(userIdInteger) {
+  return getUserId(username, scClientId).then(function(userIdInteger) {
     console.log('userIdInteger', userIdInteger);
     var favoritesUrl = 'http://api.soundcloud.com/users/' + userIdInteger
       + '/favorites.json?client_id=' + scClientId;
@@ -22,17 +20,31 @@ function findUserFavorites(username, scClientId) {
 }
 
 // helpers
-function buildResolveUrl(username, scClientId) {
-  return 'http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/' +
-    username + '&client_id=' + scClientId;
-}
 
-function getUserId(resolveUrl) {
-  return request.getAsync(resolveUrl).then(function(responseArr){
+/**
+ * Finds the integer user id associated with the user (necessary for fetching
+ * favorites)
+ *
+ * @params:
+ * {username} (the user's soundcloud username. ie - christianmonaghan)
+ * {scClientId} (this is an app id specific to this application. found in config.js)
+ */
+function getUserId(username, scClientId) {
+  var resolveUrl = buildResolveUrl(username, scClientId);
+  var promise = request.getAsync(resolveUrl).then(function(responseArr){
     var body = responseArr[0].body;
     body = JSON.parse(body);
     return body.id;
   });
+
+  return promise;
+}
+
+// meta-helpers
+
+function buildResolveUrl(username, scClientId) {
+  return 'http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/' +
+    username + '&client_id=' + scClientId;
 }
 
 module.exports = findUserFavorites;
