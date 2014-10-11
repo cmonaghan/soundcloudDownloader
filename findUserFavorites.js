6b91444650a422ff649baf230cf828c735b33ecf
Christian Monaghan <christian.monaghan@gmail.com>
2014-10-10 23:21:53 -0700

    Adds getUserId helper

--- findUserFavorites.js
+++ findUserFavorites.js
@@ -7,11 +7,8 @@ function findUserFavorites(username, scClientId) {
   // finds the integer user id associated with the username
   var resolveUrl = buildResolveUrl(username, scClientId);
 
-  return request.getAsync(resolveUrl).then(function(responseArr){
-    var body = responseArr[0].body;
-    body = JSON.parse(body);
-    return body.id;
-  }).then(function(userIdInteger) {
+  return getUserId(resolveUrl).then(function(userIdInteger) {
+    console.log('userIdInteger', userIdInteger);
     var favoritesUrl = 'http://api.soundcloud.com/users/' + userIdInteger
       + '/favorites.json?client_id=' + scClientId;
 
@@ -28,6 +25,14 @@ function findUserFavorites(username, scClientId) {
 function buildResolveUrl(username, scClientId) {
   return 'http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/' +
     username + '&client_id=' + scClientId;
+}
+
+function getUserId(resolveUrl) {
+  return request.getAsync(resolveUrl).then(function(responseArr){
+    var body = responseArr[0].body;
+    body = JSON.parse(body);
+    return body.id;
+  });
 }
 
 module.exports = findUserFavorites;
