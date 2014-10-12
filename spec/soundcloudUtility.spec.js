b9bb950152e58723f437df83aa7f49c1a1cb3307
Christian Monaghan <christian.monaghan@gmail.com>
2014-10-11 17:13:42 -0700

    Adds asynchronous test of getUserId

--- spec/soundcloudUtility.spec.js
+++ spec/soundcloudUtility.spec.js
@@ -1,7 +1,22 @@
+var soundcloudUtility = require('../soundcloudUtility');
+var config = require('../config');
 
-
-describe('findUserFavorites', function() {
+describe('soundcloudUtility', function() {
   it('should pass a test', function() {
     expect(true).toBe(true);
+  });
+  describe('#getUserId', function() {
+    var scUsername = 'christianmonaghan';
+    var scClientId = config.scClientId;
+    var returnedUserId;
+    beforeEach(function(done) {
+      soundcloudUtility.getUserId(scUsername, scClientId).then(function(userId) {
+        returnedUserId = userId;
+        done();
+      });
+    });
+    it('should return a user id', function() {
+      expect(returnedUserId).toBe(4071686);
+    });
   });
 });
